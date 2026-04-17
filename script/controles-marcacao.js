69441b4ff7571685eef703b7d46e83e51803e60f
Jorge Dantas <jatad.tech@gmail.com>
2026-04-17 03:19:07 -0300

    Exercicio03-concluido

--- script/controles-marcacao.js
+++ script/controles-marcacao.js
@@ -1,10 +1,11 @@
-  // Exercício 0: ocultar/mostrar marcações
+// Exercício 0: ocultar/mostrar marcaçõess
   const checkboxVisibilidadeMarcacoes = document.querySelector('#visibilidade-das-marcacoes');
   checkboxVisibilidadeMarcacoes.addEventListener('input', () => {
     document.body.classList.toggle(checkboxVisibilidadeMarcacoes.value, checkboxVisibilidadeMarcacoes.checked);
   });
 
   //Megaexercício 2: Selecionar marcação e preencher controles
+  let marcacaoSelecionada = document.querySelector('.marcacao.selecionada');
 
   function atualizaControles(marcacaoEl) {
 
@@ -26,14 +27,49 @@
     document.querySelector(`input[name="formato-da-marcacao"][value="${formato}"]`).checked = true;
   }
 
+    if (marcacaoSelecionada) {
+    atualizaControles(marcacaoSelecionada);
+     }
+
     //Selecionar uma marcação: quando alguma .marcacao sofrer um click, 
     // remova a classe selecionada de quem é a marcação atual e insira essa mesma classe naquela que foi alvo do click
-    marcacoes.forEach(function (marcacao) {
-      marcacao.addEventListener('click', function () {
-        marcacoes.forEach(m => m.classList.remove('selecionada'));
+    //const marcacoes = document.querySelectorAll('.marcacao');
+    for (const marcacao of marcacoes) {
+      marcacao.addEventListener('click', () => {
+        if (marcacaoSelecionada) {
+          marcacaoSelecionada.classList.remove('selecionada');
+        }
         marcacao.classList.add('selecionada');
+        marcacaoSelecionada = marcacao;
         atualizaControles(marcacao);
       });
-    });
-    const marcacaoSelecionada = document.querySelector('.marcacao.selecionada');
-    if (marcacaoSelecionada) atualizaControles(marcacaoSelecionada);
+    }
+
+    //Megaexercício 3: Controles alteram marcação
+
+      function atualizaMarcacao(marcacaoEl) {
+
+        //Campos x, y, largura e altura: devem alterar o style da marcação - Lembre-se de concatenar o valor numérico do campo com a unidade de medida px
+        marcacaoEl.style.left = document.querySelector('#x-da-marcacao').value + 'px';
+        marcacaoEl.style.top = document.querySelector('#y-da-marcacao').value + 'px';
+        marcacaoEl.style.width = document.querySelector('#largura-da-marcacao').value + 'px';
+        marcacaoEl.style.height = document.querySelector('#altura-da-marcacao').value + 'px';
+
+        //Campos título, conteúdo e cor: definem atributos de dados da marcação
+        marcacaoEl.dataset.titulo = document.querySelector('#titulo-da-marcacao').value;
+        marcacaoEl.dataset.conteudo = document.querySelector('#conteudo-da-marcacao').value;
+        marcacaoEl.dataset.cor = document.querySelector('#cor-da-marcacao').value;
+
+        const formatoSelecionado = document.querySelector('input[name="formato-da-marcacao"]:checked').value;
+        marcacaoEl.classList.remove('formato-oval', 'formato-retangular');
+        marcacaoEl.classList.add(formatoSelecionado);
+      }
+
+      const camposMarcacao = document.querySelectorAll('input:not([type="checkbox"]), textarea');
+      for (const input of camposMarcacao) {
+        input.addEventListener('input', () => {
+          if (marcacaoSelecionada) {
+            atualizaMarcacao(marcacaoSelecionada);
+          }
+        });
+      }
